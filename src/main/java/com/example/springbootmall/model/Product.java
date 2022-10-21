c22c38755dab0ade59f5a200c0191c4d2132a8b1
LukeKaiQQ <enjoylife88911@gmail.com>
2022-10-21 23:24:49 +0800

    商品功能-實作查詢商品

---
+++ src/main/java/com/example/springbootmall/model/Product.java
@@ -0,0 +1,20 @@
+package com.example.springbootmall.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class Product {
+    private Integer productId;
+    private String productName;
+    private String category;
+    private String imageUrl;
+    private Integer price;
+    private Integer stock;
+    private String description;
+    private Date createdDate;
+    private Date lastModifiedDate;
+}
